ec3fb3373da1efb38fc7a1f50af48394e4952ab0
techzain <mdalimurtaza78689@gmail.com>
2025-02-18 11:23:18 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @zack heary 
+# Subscribe YouTube Channel For Amazing Bot 
+# Ask Doubt on telegram @zack heary 
 
 from os import environ
 
